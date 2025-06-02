b452bec464359d008d5e37504d74180ff190e2d2
D Roelvink <dennis.roelvink@gmail.com>
2025-06-02 21:06:02 +0200

    1.0.6

--- Runtime/SteamSettings.cs
+++ Runtime/SteamSettings.cs
@@ -12,7 +12,7 @@ namespace com.yak.steam
         [field: SerializeField] public uint AppId { get; private set; }
         
 #if UNITY_EDITOR
-        [MenuItem("Yak Tools/Steam/Steam Settings", false, 999)]
+        [MenuItem("Steam/Steam Settings", false, 999)]
         public static void OpenSettings() => Open();
 #endif
     }
